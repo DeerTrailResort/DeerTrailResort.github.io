100f5875e38b392d5935ad9e73bd66226ecb17af
moexx399 <brandonmoe.personal@gmail.com>
2021-01-12 20:01:16 -0600

    first commit

---
+++ src/components/Headers/LandingPageHeader.js
@@ -0,0 +1,64 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function LandingPageHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/DTR_Home.jpg") + ")",
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <h1 className="presentation-title">Deer Trail Resort</h1>
+              <div className="fog-low">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+              <div className="fog-low right">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+            </div>
+            <h2 className="presentation-subtitle text-center">
+            A Family Tradition 
+            </h2>
+          </Container>
+        </div>
+        <div
+          className="moving-clouds"
+          style={{
+            backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
+          }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default LandingPageHeader;
